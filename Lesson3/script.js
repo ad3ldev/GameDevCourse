0b97a55ccb558264f2fea17e27d49b237601896d
Adel <56457765+aadelafattah@users.noreply.github.com>
2022-07-29 20:34:13 +0200

    Lesson3 - enemy 2

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -11,20 +11,27 @@ let gameFrame = 0;
 class Enemy {
 	constructor() {
 		this.image = new Image();
-		this.image.src = "enemy1.png";
-		// this.speed = Math.random() * 4 - 2;
-		this.spriteWidth = 293;
-		this.spriteHeight = 155;
+		this.image.src = "enemy2.png";
+		this.speed = Math.random() * 4 + 1;
+		this.spriteWidth = 266;
+		this.spriteHeight = 188;
 		this.width = this.spriteWidth / 2.5;
 		this.height = this.spriteHeight / 2.5;
 		this.x = Math.random() * (CANVAS_WIDTH - this.width);
 		this.y = Math.random() * (CANVAS_HEIGHT - this.height);
 		this.frame = 0;
 		this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+		this.angle = 0;
+		this.angleSpeed = Math.random() * 0.2;
+		this.curve = Math.random() * 7;
 	}
 	update() {
-		this.x += Math.random() * 5 - 2.5;
-		this.y += Math.random() * 5 - 2.5;
+		this.x -= this.speed;
+		this.y += this.curve * Math.sin(this.angle);
+		this.angle += this.angleSpeed;
+		if (this.x + this.width < 0) {
+			this.x = canvas.width;
+		}
 		if (gameFrame % this.flapSpeed == 0) {
 			this.frame = this.frame > 4 ? 0 : this.frame + 1;
 		}
